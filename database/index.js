1f869cf7e656751158f35b67309d86936eff8982
xXMauricioHXx <mauriciosh558@gmail.com>
2019-03-30 09:57:23 -0300

    configurado as variaveis de ambiente no heroku

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").load();
 }
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://admin:VTW4G7I1ofl1CcGJ@cluster0-o6gt5.mongodb.net/test?retryWrites=true", {
+mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
   useNewUrlParser: true
 });
